55c2ef1bbc0e814532d171ea3d02d495e41b629a
sumali_ch <urs.s1@live.com>
2021-01-28 17:28:43 +0100

    22.1 - Change Version

--- GEOBOX.OSC.IM.DataCheckerTool/Properties/AssemblyInfo.cs
+++ GEOBOX.OSC.IM.DataCheckerTool/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("22.1.1.*")]
+[assembly: AssemblyVersion("22.1.2.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
