e63191d200eba6872233dc7564fcb550d77887e1
Cedric Strini <cedric.strini@geobox.ch>
2021-06-30 13:36:10 +0200

    Add Installer

--- GEOBOX.OSC.IM.DataCheckerTool/Properties/AssemblyInfo.cs
+++ GEOBOX.OSC.IM.DataCheckerTool/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("22.1.3.*")]
+[assembly: AssemblyVersion("22.2.4.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
